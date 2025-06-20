6e4ceda9666b9ee526451898d4437129280dcfe6
Hakan <143508175+KebanFiru@users.noreply.github.com>
2025-06-21 00:51:48 +0300

    signin function is added
    Signin function is added
    Signup is updated

--- backend/app.py
+++ backend/app.py
@@ -17,8 +17,9 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
+    username = db.Column(db.String(120), unique=True)
     email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(30))
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -29,6 +30,7 @@ class Todo(db.Model):
     is_done = db.Column(db.Boolean, default=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+
 @app.before_first_request
 def create_tables():
     db.create_all()
@@ -36,19 +38,57 @@ def create_tables():
 @app.route('/api/signup', methods=['POST'])
 def signup():
     data = request.get_json()
-    name = data.get('name')
+    username = data.get('username')
     email = data.get('email')
+    password = data.get('password')
 
-    if not name or not email:
+    if not username or not email:
         return jsonify({'message': 'Missing name or email'}), 400
 
     if User.query.filter_by(email=email).first():
         return jsonify({'message': 'User already exists'}), 409
 
-    new_user = User(name=name, email=email)
+    new_user = User(username=username, email=email, password = password)
     db.session.add(new_user)
     db.session.commit()
-    return jsonify({'message': f'User {name} added'}), 201
+    return jsonify({'message': f'User {username} added'}), 201
+
+
+@app.route('/api/signin', methods = ['POST'])
+def signin():
+
+    data = request.get_json()
+
+    if '@' in data.get('username'):
+
+        email =  data.get('username')
+        user = User.query.filter_by(email = email).first()
+    else:
+
+        username = data.get('username')
+        user = User.query.filter_by(username = username).first()
+
+    password = data.get('password')
+
+    if user != password:
+        
+        return jsonify({'message': 'User not found'}), 404
+    
+    return jsonify({
+        'message': 'Login successful',
+        'user_id': user.id,
+        'username': user.username
+    }), 200
+
+    
+
+@app.route('/api/addtodo', methods=['POST'])
+def addtodo():
+    pass
+
+@app.route('/api/removetodo', methods=['POST'])
+def removetodo():
+    pass
 
 # 🔹 Run app
 if __name__ == '__main__':
